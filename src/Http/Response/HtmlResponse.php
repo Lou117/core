d36fe9b9eddb708b5585101712ff494b7c7f204c
Lou117 <lou117@users.noreply.github.com>
2017-08-20 13:16:30 +0200

    Add HtmlResponse

---
+++ src/Http/Response/HtmlResponse.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sylvain Glaçon
+ * Date: 29/06/2017
+ * Time: 16:11
+ */
+namespace Lou117\Core\Http\Response;
+
+class HtmlResponse extends AbstractResponse
+{
+    /**
+     * @see AbstractResponse::$contentMimeType
+     */
+    protected $contentMimeType = 'text/html';
+
+    /**
+     * Sets HTTP response body, forcing passed value to string type using settype().
+     * @see AbstractResponse::setBody()
+     * @see settype()
+     * @param mixed $body - HTTP response body.
+     * @return AbstractResponse
+     */
+    public function setBody($body): AbstractResponse
+    {
+        $this->body = $body;
+        settype($this->body, 'string');
+        return $this;
+    }
+}
